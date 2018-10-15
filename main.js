99aa0a7fcc13afbd98f79e0408bc4d7da09e9958
J. Junquera <javier.junquera.sanchez@gmail.com>
2018-10-15 16:15:30 +0200

    updating template values

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ function loadPage(){
     console.log("#contador value (val): ", $('#contador').val());
     console.log("#contadiv value (text): ", $('#contadiv').text());
 
-    var contador = $('#contador').val();
+    var contador = Number.parseInt($('#contador').val());
     $('#contador').val(contador + 1);
+
+    var contadiv = Number.parseInt($('#contadiv').text());
+    $('#contadiv').text(contador + 1);
 
   });
 
