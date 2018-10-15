815201b01015ae70f808a1982b39fda3eca26f6b
J. Junquera <javier.junquera.sanchez@gmail.com>
2018-10-15 16:35:34 +0200

    updating multiple jquery query results

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
+var incrementValue = 2;
+
+function tablaMult(){
+
+  var contador = Number.parseInt($('#contador').val());
+
+  // Recorremos los factores
+  $('#tabla-mult tbody tr').each(function(i, tr){
+
+    var factor = Number.parseInt($( tr ).children().first().text());
+    $( tr ).children().last().text(factor * contador);
+
+  });
+
+
+}
+function incrementaContador(){
+
+  var contador = Number.parseInt($('#contador').val());
+  $('#contador').val(contador + incrementValue);
+
+  var contadiv = Number.parseInt($('#contadiv').text());
+  $('#contadiv').text(contador + incrementValue);
+
+  tablaMult();
+
+}
 
 function loadPage(){
+
+  tablaMult();
 
   $('#incrementa').on("click", function(){
 
     console.log("#contador value (val): ", $('#contador').val());
     console.log("#contadiv value (text): ", $('#contadiv').text());
 
-    var contador = Number.parseInt($('#contador').val());
-    $('#contador').val(contador + 1);
-
-    var contadiv = Number.parseInt($('#contadiv').text());
-    $('#contadiv').text(contador + 1);
+    incrementaContador();
 
   });
+
 
 }
 
