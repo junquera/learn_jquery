96d286b4446e417081240862486f70a61d947005
J. Junquera <javier.junquera.sanchez@gmail.com>
2018-10-15 16:10:36 +0200

    val vs text

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 function loadPage(){
 
   $('#incrementa').on("click", function(){
-    alert("Incrementa");
+
+    console.log("#contador value (val): ", $('#contador').val());
+    console.log("#contadiv value (text): ", $('#contadiv').text());
+
+    var contador = $('#contador').val();
+    $('#contador').val(contador + 1);
+
   });
 
 }
